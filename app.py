b4d1ada0d69913c932513bab4eb7ad4a5b0dfd71
Evabela <eva.belanger@gmail.com>
2025-01-14 14:59:48 +0100

    rien

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 import sqlite3
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+
+def get_db_connection():
+    conn = sqlite3.connect('Projet_planetegeekdating.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+@app.route('/')
+
+def hello():
+    return ("Hello there")
