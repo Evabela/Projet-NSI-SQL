8c1ccf4a9c2515eadb01e28294e838a45b9e6a11
Evabela <eva.belanger@gmail.com>
2025-01-14 15:35:49 +0100

    ygvh

--- app.py
+++ app.py
@@ -3,14 +3,21 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+DATABASE = 'Projet_planetegeekdating.db'
+
 def get_db_connection():
-    conn = sqlite3.connect('Projet_planetegeekdating.db')
+    conn = sqlite3.connect(DATABASE)
     conn.row_factory = sqlite3.Row
     return conn
 
 @app.route('/')
-def hello():
-    return ("Hello there")
+def index():
+    conn = get_db_connection()  # Connexion à la base de données
+    data = conn.execute('SELECT * FROM informations').fetchall()  # Récupérer les données
+    conn.close()  # Fermer la connexion
+    return render_template('index.html', data=data)  # Passer les données à la page HTML
+
+
 
 if __name__ == "__main__":
     app.run()
